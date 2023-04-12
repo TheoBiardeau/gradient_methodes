73517b45610a0a4eff556941135782b6107c806e
TheoB <theo.biardeau@etu.univ-poitiers.fr>
2023-04-12 10:25:46 +0200

    Convention Pop8 correction

--- gradient_methodes.py
+++ gradient_methodes.py
@@ -15,101 +15,167 @@ from numpy import sqrt
 from numpy import arctan
 
 class DerichEdgeDetector :
+    """ 
+    DerichEdgeDetector class
+    This class allows computing the gradients along X and Y axes with Derich methode
+    Only grayscale img is accepted
+    """
     def __init__(self, img, alpha, omega) -> None:
         self.img = img
         self.alpha = alpha
         self.omega = omega
-        
+
     def coefficients_deriche(self):
+        """
+        Compute all deriche coeficient from aplha and omega parameters
+
+        Returns
+        -------
+        c : TypE
+            C coeficient.
+        k : TypE
+            a coeficient.
+        c1 : TypE
+            c1 coeficient.
+        c2 : TypE
+            c2 coeficient.
+        b1 : TypE
+            b1 coeficient.
+        b2 : TypE
+            b2 coeficient.
+        a : TypE
+            a coeficient.
+        a0 : TypE
+            a0 coeficient.
+        a1 : TypE
+            a1 coeficient.
+        a2 : TypE
+            a2 coeficient.
+        a3 : TypE
+            a3 coeficient.
+
+        """
         alpha = self.alpha
         omega = self.omega
-        c=(1-2*exp(-alpha)*cos(omega)+exp(-2*alpha))/(exp(-alpha)*sin(omega))
-        k=((1-2*exp(-alpha)*cos(omega)+exp(-2*alpha))*(pow(alpha,2)+pow(omega,2)))/(2*alpha*exp(-alpha)*sin(omega)+omega-omega*exp(-2*alpha))
+        c = (1 - 2 * exp(-alpha) * cos(omega) + exp(-2 * alpha)) / (exp(-alpha) * sin(omega))
+        k = (( 1 - 2 * exp(-alpha) * cos(omega) + exp(-2 * alpha)) \
+             * (pow(alpha, 2) + pow(omega, 2))) \
+            /(2 * alpha * exp(-alpha) * sin(omega) \
+              + omega - omega * exp(-2 * alpha))
 
-        c1=(k*alpha)/(pow(alpha,2)+pow(omega,2))
-        c2=(k*omega)/(pow(alpha,2)+pow(omega,2))
+        c1 = (k * alpha) / (pow(alpha, 2) + pow(omega, 2))
+        c2 = (k * omega) / (pow(alpha, 2) + pow(omega, 2))
 
-        b1=-2*exp(-alpha)*cos(omega)
-        b2=exp(-2*alpha)
+        b1 = -2 * exp(-alpha) * cos(omega)
+        b2 = exp(-2 * alpha)
 
-        a=-c*exp(-alpha)*sin(omega)
-        a0=c2
-        a1=(-c2*cos(omega)+c1*sin(omega))*exp(-alpha)
-        a2=a1-c2*b1
-        a3=-c2*b2
+        a = -c * exp(-alpha) * sin(omega)
+        a0 = c2
+        a1 = (-c2 * cos(omega) + c1 * sin(omega)) * exp(-alpha)
+        a2 = a1 - c2 * b1
+        a3 = -c2 * b2
+
+        return c, k, c1, c2, b1, b2, a, a0, a1, a2, a3
+
+    def filtre_deriche_recursif(self, axes):
+        """
         
-        return c,k,c1,c2,b1,b2,a,a0,a1,a2,a3
 
-    def filtre_deriche_recursif(self, posistion):
-        c,k,c1,c2,b1,b2,a,a0,a1,a2,a3 = self.coefficients_deriche()
+        Parameters
+        ----------
+        posistion : bool
+            Allows you to indicate along which axes the gradients are calculated.
+
+        Returns
+        -------
+        R : TypE
+            Gradient along ones axe.
+
+        """
+        c, k, c1, c2, b1, b2, a, a0, a1, a2, a3 = self.coefficients_deriche()
+
         image = self.img
-        if posistion == 1 : 
+
+        if axes == 1 :
             image = image.T
+
         shape = np.shape(self.img)
         n = shape[0]
         p = shape[1]
         # Gradient
         # Y+
-        Yp=zeros([n,p])
+        yp=zeros([n, p])
 
-        debut_j=0
-        #Yp[:,debut_j]=zeros([n,1])
-        debut_j=debut_j+1
-        Yp[:,debut_j]=image[:,debut_j-1]-b1*Yp[:,debut_j-1]
-        debut_j=debut_j+1
-        for j in range(debut_j,p,1):
-            Yp[:,j]=image[:,j-1]-b1*Yp[:,j-1]-b2*Yp[:,j-2]
-        #endfor
+        debut_j = 0
+        #yp[:,debut_j]=zeros([n,1])
+        debut_j = debut_j + 1
+        yp[:, debut_j] = image[:, debut_j-1] - b1 * yp[:, debut_j-1]
+        debut_j = debut_j + 1
+        for j in range(debut_j, p, 1):
+            yp[:, j] = image[:, j-1] - b1 * yp[:, j-1]-b2 * yp[:, j-2]
 
         # Y-
-        Ym=zeros([n,p])
+        ym = zeros([n, p])
 
-        fin_j=p-1
-        #Ym[:,fin_j]=zeros([n,1])
-        fin_j=fin_j-1
-        Ym[:,fin_j]=image[:,fin_j+1]-b1*Ym[:,fin_j+1]
-        fin_j=fin_j-1
-        for j in range(fin_j,-1,-1):
-            Ym[:,j]=image[:,j+1]-b1*Ym[:,j+1]-b2*Ym[:,j+2]
-        #endfor
+        fin_j = p - 1
+        #ym[:,fin_j]=zeros([n,1])
+        fin_j = fin_j - 1
+        ym[:, fin_j] = image[:,fin_j + 1] - b1 * ym[:,fin_j + 1]
+        fin_j = fin_j - 1
+        for j in range(fin_j, -1, -1):
+            ym[:,j] = image[:, j+1] - b1 * ym[:, j+1] - b2 * ym[:, j+2]
 
-        # S
-        S=a*(Yp-Ym)
-        
+        # s
+        s = a * (yp-ym)
+
         # Lissage
         # R+
-        Rp=zeros([n,p])
+        rp = zeros([n,p])
 
-        debut_i=0
-        Rp[debut_i,:]=a0*S[debut_i,:]
-        debut_i=debut_i+1
-        Rp[debut_i,:]=a0*S[debut_i,:]+a1*S[debut_i-1,:]-b1*Rp[debut_i-1,:]
-        debut_i=debut_i+1
+        debut_i = 0
+        rp[debut_i, :] = a0 * s[debut_i, :]
+        debut_i = debut_i + 1
+        rp[debut_i, :] = a0 * s[debut_i, :] + a1 * s[debut_i-1, :] - b1 * rp[debut_i-1, :]
+        debut_i = debut_i + 1
         for i in range(debut_i,n,1):
-            Rp[i,:]=a0*S[i,:]+a1*S[i-1,:]-b1*Rp[i-1,:]-b2*Rp[i-2,:]
+            rp[i, :] = a0 * s[i, :] + a1 * s[i-1, :] - b1 * rp[i-1, :] - b2 * rp[i-2, :]
         #endfor
 
         # R-
-        Rm=zeros([n,p])
+        rm = zeros([n, p])
 
-        fin_i=n-1
-        #Rm[fin_i,:]=zeros([1,p])
-        fin_i=fin_i-1
-        Rm[debut_i,:]=a2*S[fin_i+1,:]-b1*Rm[fin_i+1,:]
+        fin_i = n - 1
+        #rm[fin_i,:]=zeros([1,p])
+        fin_i = fin_i - 1
+        rm[debut_i, :] = a2 * s[fin_i+1, :] - b1 * rm[fin_i+1, :]
         fin_i=fin_i-1
         for i in range(fin_i,-1,-1):
-            Rm[i,:]=a2*S[i+1,:]+a3*S[i+2,:]-b1*Rm[i+1,:]-b2*Rm[i+2,:]
+            rm[i, :] =a2 * s[i+1, :] + a3 * s[i+2, :] - b1 * rm[i+1, :] - b2 * rm[i+2, :]
         #endfor
 
         # R
-        R=Rm+Rp
-        
-        return R
+        r = rm + rp
+
+        if axes == 1 :
+            return r.T
+
+        return r
 
     def derich_methode (self):
-        Gx = self.filtre_deriche_recursif(0)
-        GyTemp = self.filtre_deriche_recursif(1)
-        Gy = GyTemp.T
-        norme_gradient = sqrt(Gx**2 + Gy**2)
-        angle_gradient = arctan(Gy/Gx)
+        """
+        Compute the gradient
+
+        Returns
+        -------
+        norme_gradient : Array
+            Matrix containing the norm of the gradients at each point.
+        angle_gradient : Array
+            matrix containing the angle of the gradients at each point.
+
+        """
+        gx = self.filtre_deriche_recursif(0)
+        gy = self.filtre_deriche_recursif(1)
+        norme_gradient = sqrt(gx**2 + gy**2)
+        angle_gradient = arctan(gy/gx)
+
         return norme_gradient, angle_gradient
